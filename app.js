8c2131fc559ec9aefbc458531e2837d50caca87e
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2024-11-11 00:16:45 +0800

    added delete key behaviour, renamed checkIfEndOfFile -> checkFileBounds

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const editor = document.getElementById('text-editor');
 const font_size_rect = document.getElementById("measureText").getBoundingClientRect();
 const area_rect = document.getElementById("measureArea").getBoundingClientRect();
-const numcols = area_rect.width / font_size_rect.width;
+const numcols = Math.floor(area_rect.width / font_size_rect.width);
 const numrows = Math.floor(area_rect.height / font_size_rect.height);
 cell_selected_x = cell_selected_y = 0;
-background_text = "abcd";
+text = "welcome to browser text editor!";
 isInsertMode = false;
 
 function replaceAt(str, index, chr) {
@@ -40,6 +40,7 @@ function moveCursor(new_x, new_y) {
     cell_selected_x = new_x;
     cell_selected_y = new_y;
     editor.rows[new_y].cells[new_x].classList.add("selected");
+    document.title = "[" + cell_selected_x + "," + cell_selected_y + "]";
 }
 
 function moveCursorRight() {
@@ -59,10 +60,10 @@ function moveCursorDown() {
 }
 
 function writeAtCell(x, y, c) {
-    if (y * numrows + x >= background_text.length)
-        background_text = background_text + c;
+    if (y * numrows + x >= text.length)
+        text = text + c;
     else
-        background_text = replaceAt(background_text, y * numrows + x, c);
+        text = replaceAt(text, y * numrows + x, c);
 }
 
 function writeAtSelection(c) {
@@ -77,8 +78,8 @@ function render() {
     let index = 0;
     for (let row = 0; row < numrows; row++) {
         for (let col = 0; col < numcols; col++) {
-            if (index < background_text.length) {
-                render_cell(col, row, background_text[index]);
+            if (index < text.length) {
+                render_cell(col, row, text[index]);
                 index++;
             } else {
                 render_cell(col, row, '');
@@ -93,65 +94,56 @@ function update() {
     editor.rows[cell_selected_y].cells[cell_selected_x].classList.add("selected");
 }
 
-function checkIfEndOfFile() {
-    if (curr() > background_text.length) {
-        editor.rows[cell_selected_y].cells[cell_selected_x].classList.remove("selected");
-        diff = curr() - background_text.length + 1;
+function checkFileBounds() {
+    if (curr() > text.length) {
+        diff = curr() - text.length + 1;
         if (diff > numcols) {
             if (cell_selected_y > 0) {
-                cell_selected_y -= 1;
-                cell_selected_x = numcols - 1; // Move to the last cell of the previous row
+                moveCursor(numcols - 1, cell_selected_y - 1);
             }
         } else {
             if (diff > 0) {
                 if (cell_selected_x > 0) {
-                    cell_selected_x -= 1;
+                    moveCursor(cell_selected_x - 1, cell_selected_y);
                 } else {
                     if (cell_selected_y > 0) {
-                        cell_selected_y -= 1;
-                        cell_selected_x = numcols - 1;
+                        moveCursor(numcols - 1, cell_selected_y - 1);
                     }
                 }
             } else {
-                cell_selected_x = cell_selected_y = 0;
+                moveCursor(0, 0);
                 // Reached the start of the file (0,0) - stop further recursion
                 return;
             }
         }
         // Recurse only if further adjustments are still needed
-        checkIfEndOfFile();
-        editor.rows[cell_selected_y].cells[cell_selected_x].classList.add("selected");
+        checkFileBounds();
     }
 }
 
 document.body.addEventListener('keydown', (e) => {
+    e.preventDefault();
     if (e.key === 'ArrowRight' && cell_selected_x < numcols - 1) {
-        e.preventDefault();
         moveCursorRight();
-        checkIfEndOfFile();
+        checkFileBounds();
     } else if (e.key === 'ArrowLeft' && cell_selected_x > 0) {
-        e.preventDefault();
         moveCursorLeft();
-        checkIfEndOfFile();
+        checkFileBounds();
     } else if (e.key === 'ArrowDown' && cell_selected_y < numrows - 1) {
-        e.preventDefault();
         moveCursorDown();
-        checkIfEndOfFile();
+        checkFileBounds();
     } else if (e.key === 'ArrowUp' && cell_selected_y > 0) {
-        e.preventDefault();
         moveCursorUp();
-        checkIfEndOfFile();
+        checkFileBounds();
     } else if (e.key.length === 1) {
         writeAtSelection(e.key);
         update();
         moveCursorRight();
     } else if (e.key === ' ' && cell_selected_x < numcols - 1) {
-        e.preventDefault();
         writeAtSelection(' ');
         update();
         moveCursorRight();
     } else if (e.key === 'Backspace' && cell_selected_x > 0) {
-        e.preventDefault();
         moveCursorLeft();
         initialIsInsertMode = isInsertMode
         isInsertMode = true;
@@ -159,12 +151,25 @@ document.body.addEventListener('keydown', (e) => {
         isInsertMode = !initialIsInsertMode;
         update();
     } else if (e.key === 'Enter') {
-        e.preventDefault();
     } else if (e.key === 'Insert') {
-        e.preventDefault();
         isInsertMode = !isInsertMode;
+    } else if (e.key === 'Delete') {
+        initialIsInsertMode = isInsertMode
+        isInsertMode = true;
+        writeAtSelection('');
+        isInsertMode = !initialIsInsertMode;
+        update();
     }
 });
+
+function processDrop(ev) {
+    ev.preventDefault();
+    alert("a!!!");
+}
+
+function processDragOver(ev) {
+    ev.preventDefault();
+}
     
 function main() {
     update();
